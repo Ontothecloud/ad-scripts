c2a780bfa73f31404ca051e939aa3c85b01b55e7
Vik Patel <96190623+Ontothecloud@users.noreply.github.com>
2023-09-25 21:08:40 -0400

    Add files via upload

---
+++ AutomateADUserCreation.ps1
@@ -0,0 +1,10 @@
+﻿Import-Csv -Path 'C:\Employees.csv' | foreach {
+    $NewUserParameters = @{
+        'GivenName' = $_.FirstName
+        'Surname' = $_.LastName
+        'Name' = $_.UserName
+        'AccountPassword' = (ConvertTo-SecureString 'p@$$w0rd10' -AsPlainText -Force)
+    }
+
+    New-AdUser @NewUserParameters
+}
